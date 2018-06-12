250913e9ff3afef79decedc6c5f02b123d039118
mohamed shaapan <mohamed.shaapan.1@gmail.com>
2018-06-13 00:01:07 +0200

    database redesign: added support for my_orders window

--- src/routing/my_orders_routes.js
+++ src/routing/my_orders_routes.js
@@ -22,14 +22,19 @@ exports.handle_routes = function(server, database, directory_table)
 	// refresh huser account info
 	server.get('/my_orders/refresh_table', function (req, res) {
 
+		var user_id = 1;
 		// get data from user SESSION
-		/*var sql_query = "SELECT * FROM Customer WHERE Email='tmp@tmp.com' ;";
+		var sql_query = "SELECT B.title AS title, C.quantity AS quantity, C.unit_price AS price, C.date AS date "+
+						"FROM ((SELECT * FROM customer_order WHERE user_id="+user_id+" ) AS C) "+
+						"JOIN (book_info AS B) ON (C.isbn = B.isbn) ;";
+
 		database.query(sql_query, function (err, rows, fields) {
 			// handle errors
 			if (err) throw err;
 			//console.log('User: ', rows[0].Title);
+			//console.log(rows[0]);
 			res.end(JSON.stringify(rows));
-		});*/
+		});
 
 	});
 
